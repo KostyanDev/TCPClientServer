63d53bcec9f85c3f16799a7cfaee5c0ddf0d91d2
Константин Овчаренко <wowbufi@gmail.com>
2020-09-22 11:07:31 +0300

    New server

---
+++ Server/Command.go
@@ -0,0 +1,18 @@
+package main
+
+type commandID int
+
+const (
+	NICK commandID = iota
+	JOIN
+	ROOMS
+	MSG
+	QUIT
+	MEMBERS
+)
+
+type command struct{
+	id commandID
+	client *client
+	args []string
+}
